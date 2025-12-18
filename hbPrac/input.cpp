930a648122cb73852674c2a216684a399731b3eb
Ompal Singh <yoyo@op.com>
2025-12-18 23:44:10 +0530

    Scanf and Printf operations in c++

---
+++ hbPrac/input.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    string x;
+
+    // cin >> a >> b >> x;
+
+    // cout << "The inputs were: \n" << a << "\n" << b << "\n" << x;
+    scanf ("Numbers: %d %d", &a, &b);
+
+    printf ("The numbers entered were: %d %d\n", a, b);
+
+    return 0;
+}
